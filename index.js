101366e00498e160adfd0d882f51d106f545c319
Herren Yann Cédric <yann.herren@students.bfh.ch>
2025-05-02 22:41:22 +0200

    fix stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mainAudio = new Audio("assets/sounds/main.mp3");
 mainAudio.loop = true;
-mainAudio.volume = 0;
+mainAudio.volume = 0.8;
 const playOverlay = document.querySelector(".play-overlay");
 playOverlay.onclick = function () {
     mainAudio.play();

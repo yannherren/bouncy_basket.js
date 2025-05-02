05558edc1b861d80ec5b1dd73f82f3b324e50657
Yann Cédric Herren <yann.herren@students.bfh.ch>
2025-05-02 19:11:17 +0200

    fix ios bug

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ ball.addEventListener("touchstart", function (e) {
     ballDraggingOffsetX = (e.touches ? e.touches[0].clientX : e.clientX) - ball.offsetLeft;
     ballDraggingOffsetY = (e.touches ? e.touches[0].clientY : e.clientY) - ball.offsetTop;
 })
-ball.addEventListener("touchend", function () {
+document.addEventListener("touchend", function () {
     e.preventDefault();
     ballDragging = false;
     vXLast = lastMouseMovementX * mouseThrowStrengthFactor;

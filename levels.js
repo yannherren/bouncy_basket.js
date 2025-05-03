789b9c8e99c95efeed5c73b624ae8c9fcaf59882
Herren Yann Cédric <yann.herren@students.bfh.ch>
2025-05-03 17:33:35 +0200

    change levels

--- levels.js
+++ levels.js
@@ -4,41 +4,41 @@ export const levels = [
     ball: "assets/basketballs/ball1.png",
     gym: "assets/gyms/hall1.png",
     requiredScore: 0,
-    addedSeconds: 60
+    addedSeconds: 80
   },
   {
     name: "Hobbyist",
     ball: "assets/basketballs/ball2.png",
     gym: "assets/gyms/hall2.png",
     requiredScore: 10,
-    addedSeconds: 60
+    addedSeconds: 70
   },
   {
     name: "Amateur",
     ball: "assets/basketballs/ball3.png",
     gym: "assets/gyms/hall3.png",
     requiredScore: 30,
-    addedSeconds: 50
+    addedSeconds: 60
   },
   {
     name: "Pro",
     ball: "assets/basketballs/ball4.png",
     gym: "assets/gyms/hall4.png",
     requiredScore: 60,
-    addedSeconds: 40
+    addedSeconds: 50
   },
   {
     name: "Champion",
     ball: "assets/basketballs/ball5.png",
     gym: "assets/gyms/hall5.png",
     requiredScore: 100,
-    addedSeconds: 30
+    addedSeconds: 40
   },
   {
     name: "All-Star",
     ball: "assets/basketballs/ball6.png",
     gym: "assets/gyms/hall6.png",
     requiredScore: 150,
-    addedSeconds: 20
+    addedSeconds: 30
   }
 ];
